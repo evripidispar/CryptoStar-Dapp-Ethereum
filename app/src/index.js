e8e39bf9ace95f031aa6203ef2d11d7dabcb2f79
evripidispar <evripar@umd.edu>
2019-04-17 02:28:12 -0400

    New compile

--- app/src/index.js
+++ app/src/index.js
@@ -39,7 +39,7 @@ const App = {
     App.setStatus("New Star Owner is " + this.account + ".");
   },
 
-  // Implement Task 4 Modify the front end of the DAPP for this project
+  // Implement Task 4 Modify the front end of the DAPP
   lookUp: async function (){
     
   }
